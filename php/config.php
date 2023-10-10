2147767d89ebf8ead91f7bf06f655c8d08ecfc39
edgarEMR <78576962+edgarEMR@users.noreply.github.com>
2023-10-10 15:03:33 -0600

    Update config.php

--- php/config.php
+++ php/config.php
@@ -1,18 +1,18 @@
 <?php
     return [
         'host' => 'localhost',
-        'database' => 'monteterra', //id19690615_monteterra_prod
-        'user' => 'root', //id19690615_root2
-        'password' => '', // 7ULbW]jCVR+cBWF?
+        'database' => 'monteterra',
+        'user' => 'root',
+        'password' => '',
         'charset' => 'utf8mb4',
         'current_date' => '2022-10-22'
     ];
 
     // return [
     //     'host' => 'localhost',
-    //     'database' => 'id19690615_monteterra_prod', //monteterra
-    //     'user' => 'id19690615_root2', //root
-    //     'password' => '@s6TZm@nh]wpIxr3', // @s6TZm@nh]wpIxr3
+    //     'database' => 'id19690615_monteterra_dev', //id19690615_monteterra_prod
+    //     'user' => 'id19690615_root', //id19690615_root2
+    //     'password' => '7ULbW]jCVR+cBWF?', // 7ULbW]jCVR+cBWF?
     //     'charset' => 'utf8mb4',
     //     'current_date' => '2022-10-22'
     // ];
